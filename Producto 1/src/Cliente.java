a8b330bf5656f02026499ace79f1a57eebb269f4
rogermohino <rmohinob@uoc.edu>
2025-03-10 17:34:15 +0100

    Cliente.java

--- Producto 1/src/Cliente.java
+++ Producto 1/src/Cliente.java
@@ -1,3 +1,7 @@
+/**
+ * Representa un cliente abstracto con atributos básicos como nombre, domicilio,
+ * NIF y correo electrónico.
+ */
 public abstract class Cliente {
     // Atributos
     protected String nombre;
@@ -5,7 +9,14 @@ public abstract class Cliente {
     protected String nif;
     protected String email;
 
-    // Constructor
+    /**
+     * Constructor de la clase Cliente.
+     * 
+     * @param nombre Nombre del cliente.
+     * @param domicilio Domicilio del cliente.
+     * @param nif Número de Identificación Fiscal del cliente.
+     * @param email Correo electrónico del cliente.
+     */
     public Cliente(String nombre, String domicilio, String nif, String email) {
         this.nombre = nombre;
         this.domicilio = domicilio;
@@ -13,40 +24,76 @@ public abstract class Cliente {
         this.email = email;
     }
 
-    // Getters y Setters
+    /**
+     * Obtiene el nombre del cliente.
+     * 
+     * @return Nombre del cliente.
+     */
     public String getNombre() {
         return nombre;
     }
-
+    /**
+     * Establece el nombre del cliente.
+     * 
+     * @param nombre Nuevo nombre del cliente.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    /**
+     * Obtiene el domicilio del cliente.
+     * 
+     * @return Domicilio del cliente.
+     */
     public String getDomicilio() {
         return domicilio;
     }
-
+    /**
+     * Establece el domicilio del cliente.
+     * 
+     * @param domicilio Nuevo domicilio del cliente.
+     */
     public void setDomicilio(String domicilio) {
         this.domicilio = domicilio;
     }
-
+    /**
+     * Obtiene el NIF del cliente.
+     * 
+     * @return NIF del cliente.
+     */
     public String getNif() {
         return nif;
     }
-
+    /**
+     * Establece el NIF del cliente.
+     * 
+     * @param nif Nuevo NIF del cliente.
+     */
     public void setNif(String nif) {
         this.nif = nif;
     }
-
+    /**
+     * Obtiene el correo electrónico del cliente.
+     * 
+     * @return Correo electrónico del cliente.
+     */
     public String getEmail() {
         return email;
     }
-
+    /**
+     * Establece el correo electrónico del cliente.
+     * 
+     * @param email Nuevo correo electrónico del cliente.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
-    // Método toString
+    /**
+     * Devuelve una representación en cadena del cliente.
+     * 
+     * @return Representación en cadena del cliente.
+     */
     @Override
     public String toString() {
         return String.format("Cliente {nombre='%s', domicilio='%s', nif='%s', email='%s'}", nombre, domicilio, nif, email);
